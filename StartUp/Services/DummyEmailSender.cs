c152122b9250463fc7de512e63f1027cb1a0a87d
NourYassser <nouryasser08@gmail.com>
2025-08-30 12:11:05 +0300

    first commit

---
+++ StartUp/Services/DummyEmailSender.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using StartUp.Models;
+
+namespace StartUp.Services
+{
+    public class DummyEmailSender : IEmailSender<ApplicationUser>
+    {
+        public Task SendConfirmationLinkAsync(ApplicationUser user, string email, string confirmationLink)
+        {
+            Console.WriteLine($"[CONFIRMATION LINK] {confirmationLink}");
+            return Task.CompletedTask;
+        }
+
+        public Task SendPasswordResetLinkAsync(ApplicationUser user, string email, string resetLink)
+        {
+            Console.WriteLine($"[RESET LINK] {resetLink}");
+            return Task.CompletedTask;
+        }
+
+        public Task SendPasswordResetCodeAsync(ApplicationUser user, string email, string resetCode)
+        {
+            Console.WriteLine($"[RESET CODE] {resetCode}");
+            return Task.CompletedTask;
+        }
+    }
+}
